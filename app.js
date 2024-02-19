75ec7dcf7c907ae38b6f99c079808217450e56dc
Sophie Y <javachiphi@gmail.com>
2024-02-19 14:53:07 +0800

    add edit view and endpoint. use method override

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Spot = require('./models/spot');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/urbanx');
 
@@ -16,6 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -43,6 +45,26 @@ app.get('/spots/:id', async (req, res) => {
   const spot = await Spot.findById(id);
 
   res.render('spots/show', { spot });
+});
+
+app.get('/spots/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const spot = await Spot.findById(id);
+
+  res.render('spots/edit', { spot });
+});
+
+app.put('/spots/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, location } = req.body;
+
+  const spot = await Spot.findByIdAndUpdate(
+    id,
+    { title, location },
+    { runValidators: true, new: true }
+  );
+
+  res.redirect(`/spots/${spot._id}`);
 });
 
 app.get('/createSpot', async (req, res) => {
