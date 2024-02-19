b8933e3e67b89d88776ca063c982f87ff01e2254
Sophie Y <javachiphi@gmail.com>
2024-02-19 12:26:57 +0800

    seed fake data

--- seed/index.js
+++ seed/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Spot = require('../models/spot');
 const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/urbanx');
 
@@ -10,12 +11,15 @@ db.once('open', () => {
   console.log('Database connected');
 });
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
   await Spot.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const spots = new Spot({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
     });
 
     await spots.save();
