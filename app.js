565b77388a9bd56b76b092b22ccd07a0d76dcfe9
Sophie Y <javachiphi@gmail.com>
2024-02-19 16:05:39 +0800

    use ejsmate for layout

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Spot = require('./models/spot');
 const methodOverride = require('method-override');
+const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/urbanx');
 
@@ -13,11 +15,13 @@ db.once('open', () => {
   console.log('Database connected');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+// app.use = use every single request
 
 app.get('/', (req, res) => {
   res.render('home');
