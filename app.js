c41cb02c87ab0603eb29ba5e6595d169a8a2841c
Sophie Y <javachiphi@gmail.com>
2024-02-20 13:51:44 +0800

    use public folder to serve js

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/spots', spots);
 app.use('/spots/:id/reviews', reviews);
