9634a99c6ea8f7226a5307cfd17a6947279c25c0
Sophie Y <javachiphi@gmail.com>
2024-02-20 11:07:06 +0800

    cascade delete: when spot deleted, its reviews also

--- models/spot.js
+++ models/spot.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const spotSchema = new Schema({
   title: String,
@@ -7,6 +8,17 @@ const spotSchema = new Schema({
   description: String,
   location: String,
   reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+});
+
+// query middleware
+spotSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model('Spot', spotSchema);
