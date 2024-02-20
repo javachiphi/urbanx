417e59dd18d9417f251ada95cd5d06373c440d14
Sophie Y <javachiphi@gmail.com>
2024-02-20 17:18:50 +0800

    log in after register

--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,14 @@ router.get('/register', (req, res) => {
 
 router.post(
   '/register',
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+      });
       req.flash('success', 'Welcome to urbanX!');
       res.redirect('/spots');
     } catch (e) {
