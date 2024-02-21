dba9ed457915800cdbdd3fdc37ab9d07bca2d5c9
Sophie Y <javachiphi@gmail.com>
2024-02-21 09:44:47 +0800

    refactor: review controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,41 +4,15 @@ const Spot = require('../models/spot');
 const Review = require('../models/review');
 const catchAsync = require('../utils/catchAsync');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const reviews = require('../controllers/reviews');
 
-router.post(
-  '/',
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    console.log('hey');
-    const { id } = req.params;
-    const spot = await Spot.findById(id);
-
-    const { body, rating } = req.body;
-    const review = new Review({ body, rating });
-    review.author = req.user._id;
-    await review.save();
-    spot.reviews.push(review);
-    await spot.save();
-    req.flash('success', 'Review created successfully!');
-    res.redirect(`/spots/${spot._id}`);
-  })
-);
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete(
   '/:reviewId',
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Spot.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-
-    req.flash('success', 'Review deleted successfully!');
-    res.redirect(`/spots/${id}`);
-
-    // res.send('delete me');
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
