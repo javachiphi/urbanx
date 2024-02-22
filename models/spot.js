eb6a11638b8cba4fdd4085a0bed0e28e6c80fad4
Sophie Y <javachiphi@gmail.com>
2024-02-22 11:17:23 +0800

    add map control (zoom in zoom out)

--- models/spot.js
+++ models/spot.js
@@ -41,7 +41,7 @@ const spotSchema = new Schema(
 spotSchema.virtual('properties.popUpMarkup').get(function () {
   return `
   <strong><a href="/spots/${this._id}">${this.title}</a><strong>
-  <p>${this.description.substring(0, 20)}...</p>`;
+  <p>${this.description?.substring(0, 20)}...</p>`;
 });
 
 // query middleware
