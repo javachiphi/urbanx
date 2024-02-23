e2a131204b8d29c71a4b7ede4aca0b59c15349fd
Sophie Y <javachiphi@gmail.com>
2024-02-23 17:20:13 +0800

    turn on secure

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ module.exports.sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    // secure: true,
+    secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
