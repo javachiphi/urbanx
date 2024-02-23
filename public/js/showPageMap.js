45b0bbc53685fc815b3ad6de585faf4214d81925
Sophie Y <javachiphi@gmail.com>
2024-02-23 14:47:27 +0800

    page styling

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,26 +1,13 @@
 mapboxgl.accessToken = mapToken;
 
-function escapeNewLinesAndCarriageReturns(jsonString) {
-  return jsonString.replace(/\n/g, '\\n').replace(/\r/g, '\\r');
-}
-
-const spotFormatted = escapeNewLinesAndCarriageReturns(spot);
-
-const coordinates = JSON.parse(spotFormatted).geometry.coordinates;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: coordinates, // starting position [lng, lat]
+  center: JSON.parse(coordinates), // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
 const marker1 = new mapboxgl.Marker()
-  .setLngLat(coordinates)
-  .setPopup(
-    new mapboxgl.Popup({ offset: 25 }).setHTML(
-      `<h4>${JSON.parse(spotFormatted).title}</h4><p>${
-        JSON.parse(spotFormatted).location
-      }</p>`
-    )
-  )
+  .setLngLat(JSON.parse(coordinates))
   .addTo(map);
